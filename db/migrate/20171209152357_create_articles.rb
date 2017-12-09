e01a57e4c7a35c6a9a7dd43b8d25c90b6fbb3e77
emilesu <emilesu@qq.com>
2017-12-09 23:37:30 +0800

    add article modle and routes

---
+++ db/migrate/20171209152357_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[5.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :intro
+      t.string :thunbnail
+      t.text :description
+      t.timestamps
+    end
+  end
+end
