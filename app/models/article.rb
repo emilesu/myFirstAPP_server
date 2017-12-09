26d06a246c332b2a7998324089b9292e09a98e66
emilesu <emilesu@qq.com>
2017-12-10 00:44:35 +0800

    complete article form validates

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :intro, presence: true
+  validates :description, presence: true
+  validates :thunbnail, presence: true
 end
