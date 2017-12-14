da99abd8bb5ffee7b0118a8837f3b39414edbf52
emilesu <emilesu@qq.com>
2017-12-14 09:26:05 +0800

    add reviews route

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,21 @@ Rails.application.routes.draw do
   # API 路由
   namespace :api, :defaults => { :format => :json } do
     namespace :v1 do
+      # article api
       get "/articles" => "articles#index", :as => :articles
       get "/articles/:id" => "articles#show", :as => :article
+
+      # review api
+      get "/articles/:article_id/reviews" => "reviews#index", :as => :reviews
+      post "/articles/:article_id" => "reviews#create", :as => :create_review
     end
   end
 
   # 后台路由
   namespace :admin do
-    resources :articles
+    resources :articles do
+      resources :reviews
+    end
   end
 
 end
