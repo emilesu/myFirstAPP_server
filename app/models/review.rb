3202698c45cad7b6efb1a1168a263e492bc496ec
emilesu <emilesu@qq.com>
2017-12-14 21:39:48 +0800

    review user is optional

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   validates :content, presence: true
 
-  # 与 user 和 article 关系
-  belongs_to :user
+  # 与 user 和 article 关系,  user 为可选项
+  belongs_to :user, :optional => true
   belongs_to :article
 end
